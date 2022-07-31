bd4a9ff8a3e932ccc6c2800f752c2e48aaa1fe73
x <x@mail.com>
2022-07-31 04:33:34 -0400

    ForEachArreglosClases

---
+++ domain/Persona.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author asosadjpasdijasixapoapxsasxas
+ */
+public class Persona {
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + '}';
+    }
+    
+    private String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
---
+++ test/TestForEach.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+import domain.Persona;
+
+/**
+ *
+ * @author asosadjpasdijasixapoapxsasxas
+ */
+public class TestForEach {
+    
+    
+    public static void main(String[] args) {
+        Persona personas[] = {new Persona("Pancho"),new Persona("Pedro"),new Persona("Esteban Dido"),new Persona("Rosa Meltrozo")};
+        //este es un ejemplo de un for each con objectos
+        for(Persona persona: personas){
+            System.out.println("edad ="+persona);
+        }
+    }
+    
+}
